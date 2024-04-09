6a410373636df1fc50e0a6898430e220eb39593d
Ammar Haddad <alhaddadam@gmail.com>
2024-04-09 14:09:53 -0400

    add the countdown clock

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import FlipClockCountdown from "@leenguyen/react-flip-clock-countdown";
+import "@leenguyen/react-flip-clock-countdown/dist/index.css";
+
 import "./App.css";
 
 function App() {
-  return <div className="container">
-  <div className="clock-content">
-    <h1>Coming Soon</h1>
-    <h3>Question? please contact me through my personal portfolio webpage <a href="https://ammarhaddad.com/">ammarhaddad.com</a></h3>
-  </div>
-  
-  </div>;
+  return (
+    <div className="container">
+      <div className="clock-content">
+        <FlipClockCountdown
+          className="down-clock"
+          to={new Date().getTime() + 80 * 3600 * 1000 + 5000}
+          labels={["DAYS", "HOURS", "MINUTES", "SECONDS"]}
+          duration={0.5}
+        ></FlipClockCountdown>
+        <h1>Coming Soon</h1>
+        <h3>
+          Question? please contact me through my personal portfolio webpage.
+        
+        </h3>
+        <a href="https://ammarhaddad.com/">
+        <button className="button">Conatct Me</button>
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default App;
