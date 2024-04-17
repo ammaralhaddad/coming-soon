27c622ed34dde6f79cd278b2dd688ef2cf24a4ae
Ammar Haddad <alhaddadam@gmail.com>
2024-04-17 10:39:30 -0400

    add the loading bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
+import React,{useState, useEffect} from "react";
+import ClockLoader from "react-spinners/ClockLoader";
+
 import "@leenguyen/react-flip-clock-countdown/dist/index.css";
 
 import "./App.css";
 
 function App() {
+  let [loading, setLoading] = useState(false);
+
+useEffect(()=>{
+setLoading(true)
+setTimeout(()=>{
+setLoading(false)
+
+},5000)
+
+},[])
+
   return (
+
     <div className="container">
-      <div className="clock-content">
+    {
+    loading ?
+
+    <ClockLoader
+
+        color={'var(--primary-color)'}
+        loading={loading}
+        cssOverride={loading}
+        size={150}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+      :
+
+      <div className="content">
         
         <h1>Coming Soon</h1>
-        <h3>
+         <h3>
           Question? please contact me through my personal portfolio webpage.
         
         </h3>
         <a href="https://ammarhaddad.com/">
         <button className="button">Conatct Me</button>
-        </a>
+        </a> 
       </div>
+}
     </div>
+      
   );
+      
 }
 
 export default App;
